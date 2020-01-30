48917a7d3adaceffc8dd93e8ff57c401b457be83
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2020-01-30 12:09:32 +0100

    statische Methoden DegreeToRadiant() und RadiantToDegree() fertiggestellt

--- src/MyMath.java
+++ src/MyMath.java
@@ -3,5 +3,16 @@ public class MyMath {
 
 		//	statische Members (Methoden,Felder)
 	public static final double MyPi= 3.1415;
+	// würde man so nicht machen
 	public final double MyPi_NonStatic= 3.1415;
+	
+	//statische Methode
+	public static double DegreeToRadiant(double degree)
+	{
+		return degree * 2 * Math.PI/360;
+	}
+	public static double RadiantToDegree(double radiant)
+	{
+		return radiant * 360 / (2*Math.PI);
+	}
 }
