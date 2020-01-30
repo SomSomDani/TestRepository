88f935f71f67e0b9c434f7f3cc14cbc6e1228eb0
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2020-01-30 12:02:18 +0100

    Klasse MyMath mit statischen Members erzeugt und Hauptprogramm erzeugt

---
+++ src/MyMath.java
@@ -0,0 +1,7 @@
+
+public class MyMath {
+
+		//	statische Members (Methoden,Felder)
+	public static final double MyPi= 3.1415;
+	public final double MyPi_NonStatic= 3.1415;
+}
---
+++ src/StaticApp.java
@@ -0,0 +1,13 @@
+
+public class StaticApp {
+	
+	public static void main (String[] args)
+	{
+		// verwenden der statischen Konstante MyPi
+		System.out.printf("MyPi = %.2f\n",MyMath.MyPi);
+		// verwenden der nicht statischen Konstante MyPi_Nonstatic
+		MyMath myM = new MyMath();
+		System.out.printf("MyPi_NonStatic = %.2f\n",myM.MyPi_NonStatic);
+	}
+
+}
